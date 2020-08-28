81679c3bb0a016fa7cb9fd415b5194e7d37e89db
ropajoal <ropajoal@hotmail.es>
2020-08-27 21:00:03 -0400

    Estructura Alumno y comentarios

--- TP/OrdInsercion.cpp
+++ TP/OrdInsercion.cpp
@@ -1,29 +1,56 @@
 #include <iostream>
 #include <time.h>
-#define N 100
-#define LIMIT 2000
+#define MAX 35
+#define MIN 15
 
 using namespace std;
 
-void ordInsercion(int[],int);
+//Estructura de dato Alumno que contiene numero de Alumno y su peso
+struct Alumno{
+	int numero,peso;
+	Alumno(){}
+	Alumno(int n, int p){
+		numero = n;
+		peso = p;	
+	}
+};
 
-int main(){
-	srand(time(NULL));
-	int alumnos[N];
-	for(int i=0;i<N;i++)alumnos[i]=rand()%LIMIT;
-	ordInsercion(alumnos,N);
-	for(int i=0;i<N;i++)cout<<alumnos[i]<<" ";
-}
-
-void ordInsercion(int array[], int n){
-	int i, j, aux;
+//Metodo de ordenacion de Insercion adaptada a ordenar un array de tipo de dato Alumno por su atributo peso
+void ordInsercion(Alumno array[], int n){
+	int i, j;
+	Alumno aux;
 	for(int i=0;i<n;i++){
 		j=i;
 		aux=array[i];
-		while(j>0&&aux<array[j-1]){
+		while(j>0 && (aux.peso < array[j-1].peso) ){
 			array[j] = array[j-1];
 			j--;
 		}
 		array[j]=aux;
 	}
 }
+
+int main(){
+	srand(time(NULL));
+	int N;
+
+	cout <<"Ingrese numero de alumnos a procesar: ";
+	cin >> N;
+	Alumno alumnos[N];
+
+	//Puebla el array alumnos entre un rango de maximo y un mínimo
+	for(int i=0;i<N;i++)alumnos[i]= Alumno(i+1,(rand()%(MAX + 1 - MIN))+MIN);
+
+	//Muestra los numeros de los alumnos junto con sus respectivos pesos
+	cout <<"Pesos de alumnos a ordenar:" <<endl;
+	for(int i=0;i<N;i++)cout<<alumnos[i].numero<<":"<<alumnos[i].peso<<" ";
+	cout << endl;
+
+	//Ordena a los Alumnos por sus respectivos pesos mediante el metodo de Insercion
+	ordInsercion(alumnos,N);
+
+	//Muestra a todos los alumnos ya ordenados por sus pesos 
+	cout <<"Pesos ordenados de alumnos:"<<endl;
+	for(int i=0;i<N;i++)cout<<alumnos[i].numero<<":"<<alumnos[i].peso<<" ";
+}
+
